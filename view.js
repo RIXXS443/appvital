fc22e8f3ba5596bc018e161937f2015d7b43756d
RIXXS443 <98590487+RIXXS443@users.noreply.github.com>
2024-12-23 11:09:06 -0300

    Update view.js

--- view.js
+++ view.js
@@ -1,21 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
+const apiGetUrl = "https://prod-21.brazilsouth.logic.azure.com/workflows/a1f66a512b30401f837963fb67c270fb/triggers/manual/paths/invoke/obtener_registros?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=wv5unUjOMZBO6-uen9yvRsJi-ao7WAkE_pB35q_0D7k";
+
+document.addEventListener('DOMContentLoaded', async () => {
     const clientList = document.getElementById('clientList');
-    const clients = JSON.parse(localStorage.getItem('clients')) || [];
 
-    if (clients.length === 0) {
-        clientList.innerHTML = '<li>No hay clientes registrados.</li>';
-        return;
+    try {
+        const response = await fetch(apiGetUrl);
+
+        if (response.ok) {
+            const clients = await response.json();
+
+            if (clients.length === 0) {
+                clientList.innerHTML = '<li>No hay clientes registrados.</li>';
+                return;
+            }
+
+            clients.forEach(client => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <strong>(${client.Numero_Cliente})</strong> ${client.Nombre} ${client.Apellido}<br>
+                    <strong>Dirección:</strong> ${client.Direccion}<br>
+                    <strong>Teléfono:</strong> ${client.Telefono}<br>
+                    <strong>Correo:</strong> ${client.Email}<br>
+                    <strong>Municipio:</strong> ${client.Municipio}
+                `;
+                clientList.appendChild(li);
+            });
+        } else {
+            throw new Error('Error al obtener la lista de clientes.');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        clientList.innerHTML = '<li>Error al cargar los clientes.</li>';
     }
-
-    clients.forEach(client => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <strong>(${client.clientNumber})</strong> ${client.name}<br>
-            <strong>Dirección:</strong> ${client.address}<br>
-            <strong>Teléfono:</strong> ${client.phone}<br>
-            <strong>Correo:</strong> ${client.email}<br>
-            <strong>Municipio:</strong> ${client.municipality}
-        `;
-        clientList.appendChild(li);
-    });
 });
